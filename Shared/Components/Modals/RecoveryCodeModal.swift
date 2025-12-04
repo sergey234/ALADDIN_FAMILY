df442d92f857e7ca6a52d292a94ab0a506d80f61
sergey234 <sergey234@github.com>
2025-12-05 01:42:02 +0400

    fix: create shared ShareSheet component to resolve redeclaration errors

--- Shared/Components/Modals/RecoveryCodeModal.swift
+++ Shared/Components/Modals/RecoveryCodeModal.swift
@@ -269,18 +269,6 @@ struct RecoveryCodeModal: View {
     }
 }
 
-// MARK: - ShareSheet (простая версия без поддержки iPad)
-
-struct ShareSheet: UIViewControllerRepresentable {
-    let activityItems: [Any]
-    
-    func makeUIViewController(context: Context) -> UIActivityViewController {
-        UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
-    }
-    
-    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
-}
-
 // MARK: - Preview
 
 struct RecoveryCodeModal_Previews: PreviewProvider {
