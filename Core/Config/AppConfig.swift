288a853d5dd1eec109623388c0030a3c57844f2d
sergey234 <sergey234@github.com>
2026-04-08 01:36:09 +0400

    build: bump version to 137

--- Core/Config/AppConfig.swift
+++ Core/Config/AppConfig.swift
@@ -104,7 +104,7 @@ struct AppConfig {
     // MARK: - App Info
     
     static let appVersion = "1.0.0"
-    static let buildNumber = "136"
+    static let buildNumber = "137"
     static let bundleIdentifier = "family.aladdin.ios"
     static let appName = "ALADDIN"
     static let appDisplayName = "ALADDIN - AI Защита Семьи"
